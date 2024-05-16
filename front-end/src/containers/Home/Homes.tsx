13b1f595c72de598b59fc0dacab91b28f4f600fb
Michaela <michaelachan45@gmail.com>
2024-05-16 16:22:28 +0100

    media queries for home

--- front-end/src/containers/Home/Homes.tsx
+++ front-end/src/containers/Home/Homes.tsx
@@ -3,8 +3,8 @@ import "./Home.scss";
 const Home = () => {
     return(
         <div className="home">
-            <h1 className="home__title">Placeholder Title</h1>
-            <h3 className="home__description">A collection of products I've used throughout my journey</h3>
+            <h1 className="home__title">Michaela's Skincare Database🧪</h1>
+            <h3 className="home__description">A collection of products I've used...</h3>
         </div>
     )
 }
